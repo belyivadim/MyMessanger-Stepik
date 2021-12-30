e0618882befa7ded926b84a7ec7bd062c634908c
belyivadim <belyivadim01@gmail.com>
2021-12-30 22:00:55 +0200

    Added to the class Message serialization

--- ConsoleClient/Program.cs
+++ ConsoleClient/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Newtonsoft.Json;
 
 namespace MyMessanger_Stepik
 {
@@ -6,9 +7,13 @@ namespace MyMessanger_Stepik
     {
         static void Main(string[] args)
         {
-            Message msg = new Message();
-            Console.WriteLine("Start Project!");
-            Console.WriteLine(msg.ToString());
+            Message msg = new Message("Vadim", "Hello!", DateTime.UtcNow);
+            string output = JsonConvert.SerializeObject(msg);
+            Console.WriteLine(output);
+            Message deserializeMsg = JsonConvert.DeserializeObject<Message>(output);
+            Console.WriteLine(deserializeMsg);
+            //{ "UserName":"Vadim","MessageText":"Hello!","TimeStamp":"2021-12-30T19:57:41.5287026Z"}
+            //Vadim < 30.12.2021 19:57:41 >: Hello!
         }
     }
 }
